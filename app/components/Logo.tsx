d95a1e6b469854ddcf67025f256a0711b9c8892b
Igor Stepanov <stipyk1309@gmail.com>
2024-11-30 15:46:39 +0300

    feat: app init

---
+++ app/components/Logo.tsx
@@ -0,0 +1,10 @@
+function Logo() {
+	return (
+		<a href='/' className='flex items-center gap-4 z-10'>
+			<img src='/logo-light.webp' height='60' width='60' alt='Rauha logo' />
+			<span className='text-xl font-semibold text-primary-100'>Rauha</span>
+		</a>
+	)
+}
+
+export default Logo
