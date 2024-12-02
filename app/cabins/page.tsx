fbe6f06458bbe5a6efa9edd17e208e2916146487
Igor Stepanov <stipyk1309@gmail.com>
2024-12-02 16:35:20 +0300

    update: pages name, add loading file

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+export default async function CabinsPage() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users')
+	const data = await res.json()
+	console.log('data:', data)
+
 	return <div>Cabins</div>
 }
