73dad3be5adc6ae2962a1d106bb315fa9c3aeb07
Igor Stepanov <stipyk1309@gmail.com>
2024-12-09 23:56:20 +0300

    update: Logo Image quality 100%

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,20 +1,22 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import logo from "@/public/logo-light.webp";
 
 function Logo() {
-	return (
-		<Link href='/' className='flex items-center gap-4 z-10'>
-			<Image
-				src='/logo-light.webp'
-				height='60'
-				width='60'
-				alt='Rauha logo'
-				priority
-				className='rounded-full'
-			/>
-			<span className='text-xl font-semibold text-primary-100'>Rauha</span>
-		</Link>
-	)
+  return (
+    <Link href="/" className="z-10 flex items-center gap-4">
+      <Image
+        src={logo}
+        alt="Rauha logo"
+        priority
+        className="rounded-full"
+        height="60"
+        width="60"
+        quality={100}
+      />
+      <span className="text-xl font-semibold text-primary-100">Rauha</span>
+    </Link>
+  );
 }
 
 export default Logo
