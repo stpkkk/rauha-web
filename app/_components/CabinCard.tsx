99d72b414476b0c8aa3e59b096bea2570ecca6af
Igor Stepanov <stipyk1309@gmail.com>
2024-12-14 11:37:57 +0000

    feat: global and Cabins loading Spinner

--- app/_components/CabinCard.tsx
+++ app/_components/CabinCard.tsx
@@ -17,6 +17,7 @@ function CabinCard({ cabin }:CabinCardProps) {
           src={image}
           alt={`Домик ${name}`}
           fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           className="border-r border-primary-800 object-cover"
         />
       </div>
