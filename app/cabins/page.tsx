a91bb86b3781c9ff183200e7096b6cd3363480d2
Igor Stepanov <stipyk1309@gmail.com>
2024-12-14 14:37:00 +0300

    feat: fetch Cabins from Supabase

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,32 +1,37 @@
-import CabinCard from '../_components/CabinCard'
-import { CabinType } from '../_types/cabin'
+import { revalidatePath } from "next/cache";
+import CabinCard from "../_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
+import { CabinType } from "../_types/cabin";
 
 export const metadata = {
-	title: 'Домики',
-}
+  title: "Домики",
+};
 
-export default function CabinsPage() {
-	const cabins:CabinType[]= [{	id:'123',
-		name: '008',
-		maxCapacity:4,
-		regularPrice: 700,
-		discount: 100,
-		image: ''}] 
+export default async function CabinsPage() {
+  const cabins: CabinType[] = await getCabins();
+  revalidatePath("/cabins");
 
-	return <div>
-	<h1 className="text-4xl mb-5 text-accent-400 font-medium">
-		Про наши домики
-	</h1>
-	<p className="text-primary-200 text-lg mb-10">
-	Уютные, но роскошные коттеджи, расположенные в самом сердце Карелии. Представьте себе, что вы просыпаетесь и наслаждаетесь прекрасным видом на озеро, проводите дни, исследуя леса вокруг, или просто расслабляетесь в чане под звездами. Наслаждайтесь красотой природы в небольшом домике вдали от города. Идеальное место для тихого, спокойного отдыха. Добро пожаловать!
-	</p>
+  return (
+    <div>
+      <h1 className="mb-5 text-4xl font-medium text-accent-400">
+        Про наши домики
+      </h1>
+      <p className="mb-10 text-lg text-primary-200">
+        Уютные, но роскошные коттеджи, расположенные в самом сердце Карелии.
+        Представьте себе, что вы просыпаетесь и наслаждаетесь прекрасным видом
+        на озеро, проводите дни, исследуя леса вокруг, или просто расслабляетесь
+        в чане под звездами. Наслаждайтесь красотой природы в небольшом домике
+        вдали от города. Идеальное место для тихого, спокойного отдыха. Добро
+        пожаловать!
+      </p>
 
-	{cabins.length > 0 && (
-		<div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-			{cabins.map((cabin) => (
-				<CabinCard cabin={cabin} key={cabin.id} />
-			))}
-		</div>
-	)}
-</div>
+      {cabins.length > 0 && (
+        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bcxflhvvngrqocdbdafn.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
   },
 };
 
